53ab45294ac316063e2900c41546266c74ad3921
Anurag Sharma <anurag.sharma@remotestate.com>
2022-03-31 16:46:01 +0530

    Added Validations

--- Middlewares/validation.js
+++ Middlewares/validation.js
@@ -1,17 +1,61 @@
+const res = require('express/lib/response')
 const joi = require('joi')
 const schemas = require('../schema')
 const check = (schemas,property)=>{
     return (req,res,next)=>{
-        const {error} = joi.validate(req.body,schemas);
-        error == null;
-        if(error){
+        const {error} = schemas[property].validate(req.body,schemas);
+        valid=error == null;
+        if(valid){
             next()
         }else{
             const {details} = error;
-            const message = details.map(i => i.messagae).join(',');
-            console.log("error",message);
+            // console.log(details)
+            const message = details.map(i => i.message).join(',');
             res.status(422).json({error:message})
         }
     }
 }
+
+
+
+
+
+
+// const regBody = (req,res,next)=>{
+//     const result = regSchema.validate(req.body)
+//     if(result.error){
+//         res.status(400).send(result.error.details[0].message)
+//         // console.log(result.error.details[0].message)
+//     }
+//     else{
+//         next()
+//     }
+// }
+
+// const logBody = (req,res,next)=>{
+//     const result = logSchema.validate(req.body)
+//     if(result.error){
+//         res.status(400).send(result.error.details[0].message)
+//         // console.log(result.error.details[0].message)
+//     }
+//     else{
+//         next()
+//     }
+// }
+// module.exports = {regBody,logBody}
+
+
+
+
+// const check = (some,req,res,next) => {
+//     const result = some.validate(req.body)
+//     if(result.error){
+//         res.status(400).send(result.error.details[0].message)
+//         // console.log(result.error.details[0].message)
+//     }
+//     else{
+//         next()
+//     }
+// }
+
 module.exports = check
--- updatedRoutes.js
+++ updatedRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router()
 const { pool } = require('./connection')
 const authenticate = require('./Middlewares/auth')
 const { createToken } = require('./utils')
+const check = require('./Middlewares/validation')
+
+
+const schemas = require('./schema')
 
 
 
@@ -15,7 +19,41 @@ const { createToken } = require('./utils')
 
 
 
-router.post('/register',async (req, res, next) => {
+
+
+// router.post('/register',async (req, res, next) => {
+//     const { id,
+//         email,
+//         password,
+//         firstname,
+//         lastname,
+//         mobilenumber } = req.body
+
+//     try {
+//         if (id && email && password && firstname && lastname && mobilenumber) {
+//             const sql = `INSERT INTO users(id,email,password,firstname,lastname,mobilenumber) 
+//                      VALUES($1,$2,$3,$4,$5,$6) returning *`
+//             const result = await pool.query(sql, [id, email, password, firstname, lastname, mobilenumber])
+//             res.status(200).json({ message: 'User has been Registered.', data: result.rows })
+
+//         }
+//         else {
+//             res.status(400).send("Fields cannot be null.")
+//         }
+//     }
+//     catch (error) {
+//         next(err)
+//         // res.status(500).send(error.message)
+//     }
+
+
+// })
+
+
+
+
+//Routes with JOI Validation
+router.post('/register',check(schemas,'regSchema'),async(req, res, next) => {
     const { id,
         email,
         password,
@@ -24,18 +62,12 @@ router.post('/register',async (req, res, next) => {
         mobilenumber } = req.body
 
     try {
-        if (id && email && password && firstname && lastname && mobilenumber) {
-            const sql = `INSERT INTO users(id,email,password,firstname,lastname,mobilenumber) 
+        const sql = `INSERT INTO users(id,email,password,firstname,lastname,mobilenumber) 
                      VALUES($1,$2,$3,$4,$5,$6) returning *`
-            const result = await pool.query(sql, [id, email, password, firstname, lastname, mobilenumber])
-            res.status(200).json({ message: 'User has been Registered.', data: result.rows })
-            
-        }
-        else {
-            res.status(400).send("Fields cannot be null.")
-        }
+        const result = await pool.query(sql, [id, email, password, firstname, lastname, mobilenumber])
+        res.status(200).json({ message: 'User has been Registered.', data: result.rows })
     }
-    catch (error) {
+    catch (err) {
         next(err)
         // res.status(500).send(error.message)
     }
@@ -44,35 +76,67 @@ router.post('/register',async (req, res, next) => {
 })
 
 
-router.post('/login',async (req, res,next) => {
+
+
+
+
+// router.post('/login',async (req, res, next) => {
+//     const { email, password } = req.body
+//     // console.log(email,password)
+//     try {
+//         if (email && password) {
+//             const sql = `SELECT * FROM users WHERE email=$1 AND password=$2`
+//             const result = await pool.query(sql, [email, password])
+//             // console.log(result.rows[0].id)
+//             const token = await createToken(result.rows[0].id)
+//             res.status(200).json({
+//                 message: 'User Logged In Succesfully.',
+//                 token
+//             })
+
+//         }
+//         else {
+//             res.status(400).send(`email and password is required`)
+//         }
+//     }
+//     catch (error) {
+//         next(err)
+//         // res.status(400).send(`Please provide the right credentials for login.`)
+//     } 
+
+// })
+
+
+
+
+
+
+
+
+router.post('/login',check(schemas,'logSchema'),async (req, res, next) => {
     const { email, password } = req.body
     // console.log(email,password)
-    try{
-        if(email && password){
-            const sql = `SELECT * FROM users WHERE email=$1 AND password=$2`
-            const result =  await pool.query(sql,[email,password])
-            // console.log(result.rows[0].id)
-            const token  = await createToken(result.rows[0].id)
-            res.status(200).json({
-                message:'User Logged In Succesfully.',
-                token
-            })
-   
-        }
-        else{
-            res.status(400).send(`email and password is required`)
-        }
+    try {
+       
+        const sql = `SELECT * FROM users WHERE email=$1 AND password=$2`
+        const result = await pool.query(sql, [email, password])
+        // console.log(result.rows[0].id)
+        const token = await createToken(result.rows[0].id)
+        res.status(200).json({
+            message: 'User Logged In Succesfully.',
+            token
+        })
     }
-    catch(error){
+    catch (err) {
         next(err)
         // res.status(400).send(`Please provide the right credentials for login.`)
     }
-    
+
 })
 
 
 //Getting all the task with active,completed or with both the status
-router.get('/all_task',authenticate, async (req, res,next) => {
+router.get('/tasks', authenticate, async (req, res, next) => {
     // console.log('hsjkafh')
     const userId = Number(req.userId)
     // console.log(userId)
@@ -83,10 +147,10 @@ router.get('/all_task',authenticate, async (req, res,next) => {
     // console.log(complete)
     const sql = `SELECT * FROM tasks WHERE ($1::boolean IS NULL OR complete=$1) AND deleted=false AND user_id=$2`
     try {
-        const result = await pool.query(sql, [complete,userId])
+        const result = await pool.query(sql, [complete, userId])
         res.status(200).json({ data: result.rows })
     }
-    catch (error) {
+    catch (err) {
         next(err)
         // res.status(404).send(error.message)
     }
@@ -94,15 +158,15 @@ router.get('/all_task',authenticate, async (req, res,next) => {
 
 
 //Creating a new task into the list
-router.post('/create',authenticate, async (req, res,next) => {
+router.post('/create-task', authenticate, async (req, res, next) => {
     const userId = Number(req.userId)
     const sql = `INSERT INTO tasks(id,label,complete,user_id) VALUES($1,$2,$3,$4) returning *`
     try {
         const { id, label, complete } = req.body
-        const result = await pool.query(sql, [id, label, complete,userId])
+        const result = await pool.query(sql, [id, label, complete, userId])
         res.status(200).json({ message: 'Item Created Successfully.', data: result.rows })
     }
-    catch (error) {
+    catch (err) {
         next(err)
         // res.status(404).json(error.message)
     }
@@ -110,16 +174,19 @@ router.post('/create',authenticate, async (req, res,next) => {
 
 
 
+
+
+
 //Updating a task in the list
-router.put('/change/:id',authenticate,async(req,res,next)=>{
+router.put('/change-task/:id', authenticate, async (req, res, next) => {
     const { id } = req.params
-    const {label,complete} = req.body
+    const { label, complete } = req.body
     const sql = `Update tasks SET label=$2,complete=$3 WHERE id=$1`
-    try{
-        const result = await pool.query(sql,[id,label,complete])
-        res.status(200).json({message:"Item updated Successfully",data:result.rows})
+    try {
+        const result = await pool.query(sql, [id, label, complete])
+        res.status(200).json({ message: "Item updated Successfully", data: result.rows })
     }
-    catch(error){
+    catch (err) {
         next(err)
         // res.status(400).json('Please provide the right ID.')
     }
@@ -128,7 +195,7 @@ router.put('/change/:id',authenticate,async(req,res,next)=>{
 
 
 //Deleting a task from the list
-router.delete('/delete/:id',authenticate, async (req, res,next) => {
+router.delete('/delete-task/:id', authenticate, async (req, res, next) => {
     const { id } = req.params
     // const userId = Number(req.userId)
     const sql = `UPDATE tasks SET deleted=true WHERE id=$1`
@@ -136,7 +203,7 @@ router.delete('/delete/:id',authenticate, async (req, res,next) => {
         const result = await pool.query(sql, [id])
         res.status(200).json({ message: 'Item Deleted Successfully.', data: result.rows })
     }
-    catch (error) {
+    catch (err) {
         next(err)
         // res.status(404).send(error.message)
     }
